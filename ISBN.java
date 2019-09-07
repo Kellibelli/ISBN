09d0352c6871b62499fbecb2fc272c162dca0b33
Kelli Garner <32491649+Kellibelli@users.noreply.github.com>
2019-09-06 20:08:17 -0600

    Update ISBN.java

--- ISBN.java
+++ ISBN.java
@@ -1,28 +1,91 @@
+package ISBN;
+
 import java.util.Scanner;
-public class ISBN {
+public class ISBN
+{
     public static void main(String[] args) {
-                Scanner input = new Scanner(System.in);
+        ISBNVerifier verifier = new ISBNVerifier();
+        verifier.calculate10digit();
+    }
 
-                int[] numbers = new int[9];
+    public static class ISBNVerifier{
 
-                System.out.print("Enter the ISBN number: ");
+        public int[] digits;
+        public Scanner input;
+        public int  inputLength;
+        public final int ISBN_LENGTH = 10;
 
-                for (int i = 0; i < numbers.length; i++) {
-                    for (int j = 1; j < 10; j++) {
-                        numbers[i] = input.nextInt() * j;
-                    }
-                }
-                int checksum = 0;
-                for (int a = 0; a < 10; a++) {
-                    checksum += numbers[a];
-                }
-                int d10 = (checksum % 11);
-                System.out.println(d10);
+        //Constructor
+        public ISBNVerifier(){
+            digits = new int[9];
+            input = new Scanner(System.in);
+            inputLength = 0;
+        }
 
-                if (d10 == 10) {
-                    System.out.println("The ISBN's 10 number is " + numbers + "X");
-                } else {
-                    System.out.println("The ISBN'S 10 number is " + numbers);
-                }
+        void calculate10digit() {
+            int digitTen = 0;
+            int finalnumber = 0;
+            int UserInput;
+            boolean leadingZero = false;
+
+            System.out.print("Enter the ISBN number: ");
+            UserInput = input.nextInt();
+
+            leadingZero = checkForZero(UserInput);
+            parseDigits(UserInput);
+            finalnumber = CheckSum(UserInput);
+            digitTen = calcDigit10();
+
+            //Print the final number with digit 10
+            System.out.print("The ISBN-10 number is: ");
+            if (leadingZero == true){
+                System.out.print(0);
+            }
+            System.out.print(finalnumber);
+
+            if (digitTen == 10){
+                System.out.print("X");
+            }
+            else {
+                System.out.print(digitTen);
             }
         }
+
+        private void parseDigits (int UserInput){
+            for ( int i = 0; i < ISBN_LENGTH - 1 ; i++) {
+                digits[i] = UserInput % 10;
+                UserInput = UserInput / 10;
+            }
+        }
+
+        private int CheckSum(int UserInput) {
+            int finalnumber = 0;
+            for ( int i = 0; i < ISBN_LENGTH - 1 ; i++) {
+                digits[i] = UserInput % 10;
+                UserInput = UserInput / 10;
+                finalnumber += digits[i] * Math.pow(10, i);
+
+            }
+            return finalnumber;
+        }
+
+        private boolean checkForZero(int UserInput) {
+            if (UserInput < 99999999){
+                inputLength = 8;
+                digits[0] = 0;
+                return true;
+            }
+            return false;
+        }
+
+        private int calcDigit10(){
+            int digitTen = 0;
+            for ( int i = 1; i < ISBN_LENGTH -1; i++) {
+                digitTen += digits[i-1] *(ISBN_LENGTH-i);
+            }
+            digitTen = digitTen % 11;
+            return digitTen;
+        }
+    }
+}
+
